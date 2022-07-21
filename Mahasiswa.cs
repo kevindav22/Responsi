1b84d9013c8770622ffc5d66e8b96f6cc791d00c
kevindav22 <99861239+kevindav22@users.noreply.github.com>
2022-07-21 20:32:00 +0700

    Update Mahasiswa.cs
    mhs

--- Mahasiswa.cs
+++ Mahasiswa.cs
@@ -7,32 +7,10 @@ namespace ProjectMahasiswa
 {
     public class Mahasiswa
     {
-        public string nim { get; set; }
-        public string nama { get; set; }
-        public string Kelamin { get; set; }
-        public string total { get; set; }
-
-        // public void tambah(){
-        //     Console.Write("\nNota: ");
-        //     penjualan.nota = nota = Console.ReadLine();
-        //     Console.Write("\nTanggal: ");
-        //     penjualan.tanggal = tanggal = Console.ReadLine();
-        //     Console.Write("\nCustomer : ");
-        //     penjualan.nama = nama = Console.ReadLine();
-        //     Console.Write("\nJenis : ");
-        //     penjualan.jenis = jenis = Console.ReadLine();
-        //     Console.Write("\nTotal : ");
-        //     penjualan.total = total = Console.ReadLine();
-
-        //     daftarPenjualan.Add(penjualan);
-        // }
-
-        // public void tampil(){
-        //     Console.WriteLine("\nData Penjualan");
-        //     foreach(Penjualan penjual in list){
-        //         Console.WriteLine("1. {0}, {1}, {2}, {3}, {4}, {5}",penjual.nota,penjual.tanggal,penjual.nama,penjual.jenis,penjual.total);
-        //         Console.WriteLine("\n");
-        //     }
-        // }
+        // PERINTAH: lengkapi property class mahasiswa, sesuai petunjuk soal
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public string Gender { get; set; }
+        public string IPK { get; set; }
     }
 }
